f583f18aafd59f34848860b7e95454df01211aa4
Tuomas Kulmala <sufa@qscrum.com>
2017-06-23 07:18:55 +0300

    Postgre lisätty

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -12,6 +12,11 @@ import tikape.runko.database.ViestiDao;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // asetetaan portti jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
         Database database = new Database("jdbc:sqlite:opiskelijat.db");
         database.init();
 
