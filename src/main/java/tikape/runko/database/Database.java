3556871c9a4d0ebeceb79b4e7012b2b24eea0804
Tuomas Kulmala <tupaku@qscrum.com>
2017-06-25 03:17:06 +0300

    CREATE TABLE lauseiden taulujen nimen korjattu alkamaan isolla ja muuten pienillä kirjaimilla

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -71,35 +71,35 @@ public class Database {
         lista.add("CREATE TABLE Viestiketju(id SERIAL PRIMARY KEY,viestialue INT NOT NULL,otsikko VARCHAR(255),FOREIGN KEY(viestialue) REFERENCES Viestialue(id));");
         lista.add("CREATE TABLE Viesti(id SERIAL PRIMARY KEY,lahettaja VARCHAR,lahettaja_ip VARCHAR,viestiketju INT NOT NULL,viesti VARCHAR(255),viestinaika TIMESTAMP WITH TIME ZONE,FOREIGN KEY(viestiketju) REFERENCES Viestiketju(id));");
      
-        lista.add("INSERT INTO VIESTIALUE (nimi) VALUES('Ohjelmointi');");
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Miksi c++ on niin vaikeaa?')");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('aloittelija',(SELECT MAX(id)FROM VIESTIKETJU),'En ymmärrä, miksi tämä on niin vaikeaa');");
+        lista.add("INSERT INTO Viestialue (nimi) VALUES('Ohjelmointi');");
+        lista.add("INSERT INTO Viestiketju (viestialue,otsikko) VALUES((SELECT MAX(id)FROM Viestialue),'Miksi c++ on niin vaikeaa?')");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('aloittelija',(SELECT MAX(id)FROM Viestiketju),'En ymmärrä, miksi tämä on niin vaikeaa');");
         
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('chiefmaster',(SELECT MAX(id)FROM VIESTIKETJU),'Mikä siinä on niin vaikea käsittää');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'Ehkä olet tyhmä');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('teme',(SELECT MAX(id)FROM VIESTIKETJU),'Trolli trololoo');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('chiefmaster',(SELECT MAX(id)FROM Viestiketju),'Mikä siinä on niin vaikea käsittää');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM Viestiketju),'Ehkä olet tyhmä');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('teme',(SELECT MAX(id)FROM Viestiketju),'Trolli trololoo');");
         
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Python ohjeet hakusessa')");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Matti',(SELECT MAX(id)FROM VIESTIKETJU),'Mistä kannattaa etsiä Python ohjeita?');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Pythonist',(SELECT MAX(id)FROM VIESTIKETJU),'Katso online dokumentaatiosta tai Stack Overflowsta');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'No ei niitä ainakaan täältä kannata kysellä');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Matti',(SELECT MAX(id)FROM VIESTIKETJU),'Joo huomasin :D');");
+        lista.add("INSERT INTO Viestiketju (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Python ohjeet hakusessa')");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Matti',(SELECT MAX(id)FROM Viestiketju),'Mistä kannattaa etsiä Python ohjeita?');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Pythonist',(SELECT MAX(id)FROM Viestiketju),'Katso online dokumentaatiosta tai Stack Overflowsta');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM Viestiketju),'No ei niitä ainakaan täältä kannata kysellä');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Matti',(SELECT MAX(id)FROM Viestiketju),'Joo huomasin :D');");
         
-        lista.add("INSERT INTO VIESTIALUE (nimi) VALUES('Lemmikit');");
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Mitä lemmikkejä sinulla on?')");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Eemeli',(SELECT MAX(id)FROM VIESTIKETJU),'Mitä lemmikkejä teillä on kotona?');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('dogMan',(SELECT MAX(id)FROM VIESTIKETJU),'Minulla on koira');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Milli',(SELECT MAX(id)FROM VIESTIKETJU),'Otin juuri kissanpennun');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'vihaankoiria!?!');");
+        lista.add("INSERT INTO Viestialue (nimi) VALUES('Lemmikit');");
+        lista.add("INSERT INTO Viestiketju (viestialue,otsikko) VALUES((SELECT MAX(id)FROM Viestiketju),'Mitä lemmikkejä sinulla on?')");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Eemeli',(SELECT MAX(id)FROM Viestiketju),'Mitä lemmikkejä teillä on kotona?');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('dogMan',(SELECT MAX(id)FROM Viestiketju),'Minulla on koira');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Milli',(SELECT MAX(id)FROM Viestiketju),'Otin juuri kissanpennun');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM Viestiketju),'vihaankoiria!?!');");
         
-        lista.add("INSERT INTO VIESTIALUE (nimi) VALUES('Lentokoneet');");
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Tupolev vai Airbus')");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('kyselijä',(SELECT MAX(id)FROM VIESTIKETJU),'Kumpia Finnairin kannattaisi hankkia?');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('tietäjä',(SELECT MAX(id)FROM VIESTIKETJU),'Ei kumpiakaan. Ei pysy ilmassa');");
+        lista.add("INSERT INTO Viestialue (nimi) VALUES('Lentokoneet');");
+        lista.add("INSERT INTO Viestiketju (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Tupolev vai Airbus')");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('kyselijä',(SELECT MAX(id)FROM Viestiketju),'Kumpia Finnairin kannattaisi hankkia?');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('tietäjä',(SELECT MAX(id)FROM Viestiketju),'Ei kumpiakaan. Ei pysy ilmassa');");
        
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Oliko saksalaisilla helikopteri toisessa maailmansodassa')");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('hekomies',(SELECT MAX(id)FROM VIESTIKETJU),'Ei mutta Venäläisillä oli MI-24');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Araska',(SELECT MAX(id)FROM VIESTIKETJU),'Tämä kysymys ei kuulu tälle alueelle');");
+        lista.add("INSERT INTO Viestialue (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Oliko saksalaisilla helikopteri toisessa maailmansodassa')");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('hekomies',(SELECT MAX(id)FROM Viestiketju),'Ei mutta Venäläisillä oli MI-24');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Araska',(SELECT MAX(id)FROM Viestiketju),'Tämä kysymys ei kuulu tälle alueelle');");
 
         return lista;
     }
@@ -120,11 +120,11 @@ public class Database {
         lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Miksi c++ on niin vaikeaa?')");
         lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('aloittelija',(SELECT MAX(id)FROM VIESTIKETJU),'En ymmärrä, miksi tämä on niin vaikeaa');");
         
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('chiefmaster',(SELECT MAX(id)FROM VIESTIKETJU),'Mikä siinä on niin vaikea käsittää');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'Ehkä olet tyhmä');");
-        lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('teme',(SELECT MAX(id)FROM VIESTIKETJU),'Trolli trololoo');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('chiefmaster',(SELECT MAX(id)FROM VIESTIKETJU),'Mikä siinä on niin vaikea käsittää');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'Ehkä olet tyhmä');");
+        lista.add("INSERT INTO Viesti (lahettaja, viestiketju, viesti) VALUES('teme',(SELECT MAX(id)FROM VIESTIKETJU),'Trolli trololoo');");
         
-        lista.add("INSERT INTO VIESTIKETJU (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Python ohjeet hakusessa')");
+        lista.add("INSERT INTO Viestiketju (viestialue,otsikko) VALUES((SELECT MAX(id)FROM VIESTIALUE),'Python ohjeet hakusessa')");
         lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Matti',(SELECT MAX(id)FROM VIESTIKETJU),'Mistä kannattaa etsiä Python ohjeita?');");
         lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Pythonist',(SELECT MAX(id)FROM VIESTIKETJU),'Katso online dokumentaatiosta tai Stack Overflowsta');");
         lista.add("INSERT INTO VIESTI (lahettaja, viestiketju, viesti) VALUES('Anonymous',(SELECT MAX(id)FROM VIESTIKETJU),'No ei niitä ainakaan täältä kannata kysellä');");
